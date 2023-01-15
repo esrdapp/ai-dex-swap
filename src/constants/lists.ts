8012c7bf37ebacdfcdab8ff712c83d675aec012a
ESR <82104698+esrdapp@users.noreply.github.com>
2023-01-15 21:46:48 +0000

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
-// the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://dex.hpbnode.com/download/hpdex.tokenlist.json'
+// the AI-DEX Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/esrdapp/ai-dex-swap/main/public/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
