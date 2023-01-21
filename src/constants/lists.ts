30c2f042a57563a0942a5c88864fd7038a8715d4
ESR <82104698+esrdapp@users.noreply.github.com>
2023-01-21 19:22:41 +0000

    Update lists.ts

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the AI-DEX Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://tokenlist.ai-dex.net/tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/esrdapp/tokenlist-ai-dex/main/hpb.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL
